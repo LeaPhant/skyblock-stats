4f9ac13693c9874d30d386b725ce9d9d70a3e6f6
LeaPhant <mail@lea.moe>
2020-08-16 11:59:54 +0200

    Prevent Plancke from appearing on leaderboards

--- src/constants/misc.js
+++ src/constants/misc.js
@@ -1,7 +1,8 @@
 module.exports = {
     // prevent specific players from appearing in leaderboards
     blocked_players: [
-        "20934ef9488c465180a78f861586b4cf" // Minikloon (Admin)
+        "20934ef9488c465180a78f861586b4cf", // Minikloon (Admin)
+        "f025c1c7f55a4ea0b8d93f47d17dfe0f", // Plancke (Admin)
     ],
 
     item_types: [
