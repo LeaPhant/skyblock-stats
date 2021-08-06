aeaa8733484478ace03a02d88ab70b5841faab84
LeaPhant <mail@lea.moe>
2021-08-07 00:46:55 +0200

    Better item icon fix

--- src/lib.js
+++ src/lib.js
@@ -514,9 +514,6 @@ async function getItems(base64, customTextures = false, packs, cacheOnly = false
             if(item.type == 'hatccessory')
                 item.type = 'accessory';
 
-            if(item.type != null && item.type.startsWith('dungeon'))
-                item.Damage = 0;
-
             // fix custom maps texture
             if(item.id == 358){
                 item.id = 395;
