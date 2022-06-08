29586aeb16c46b56e234b8b30e0f47adf7a001e5
LeaPhant <mail@lea.moe>
2022-06-08 23:08:35 +0200

    Add additional check

--- src/lib.js
+++ src/lib.js
@@ -2657,7 +2657,7 @@ module.exports = {
                         b.kills = 0;
 
                         for (const id of b.id) {
-                            b.kills += userProfile?.bestiary[`kills_family_${id}`] ?? 0;
+                            b.kills += userProfile?.bestiary?.[`kills_family_${id}`] ?? 0;
                         }
 
                         b = { ...b, ...getBestiaryLevel(b) };
